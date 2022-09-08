e4dd8254a23d517757a486b8149c1c3c0a061a0a
Nguyễn Văn Kính <kinhnv1801@gmail.com>
2022-09-08 10:50:22 +0700

    Update end-points.js

--- js/end-points.js
+++ js/end-points.js
@@ -1,31 +1,41 @@
 class EndPointscontroller {
     serverEndPoints = [{
         name: 'argocd',
+        type: 1,
         url: '',
         token: '29OLfrflhD4CiUW7HTC7LkrHd1q_6Eojoxvjoy8qYxmKtqjj1'
     },{
         name: 'ssh',
+        type: 2,
         url: '',
         token: '2A3pcj15xBCidNr1U7fNHEvsVt4_rphzWD9zR8DL5LrMViyZ'
     }];
 
     testMakerEndPoints = [{
         name: 'administrator',
-        url: '',
-        token: '2AcXUbueVLNj8xem2wnx61ffi2J_2rty9g7EcFXYrYPr228gv'
+        type: 1,
+        url: 'https://tm-bro.github.io/admin',
     },{
         name: 'test player',
-        url: '',
-        token: '2AFtm5YtcY4aDrNdjpxRtApjuES_5hDCtABTVS7kYXPVkGXiZ'
+        type: 1,
+        url: 'https://tm-bro.github.io/test-player',
     }];
 
     constructor() {
         this.serverEndPoints.forEach(endPoint => {
             this.getEndPoint(endPoint).then(endPoint => {
-                $('.js__server-end-points').append(`
-                <tr>
-                    <td><a target="_blank" href="${endPoint.url}">${endPoint.name}</a></td>
-                </tr>`)
+                if (endPoint == 1) {
+                    $('.js__server-end-points').append(`
+                    <tr>
+                        <td><a target="_blank" href="${endPoint.url}">${endPoint.name}</a></td>
+                    </tr>`)
+                }
+                else if (endPoint == 2) {
+                    $('.js__server-end-points').append(`
+                    <tr>
+                        <td><p>${endPoint.name}: ${endPoint.url}</p></td>
+                    </tr>`)
+                }
             });
         });
         this.testMakerEndPoints.forEach(endPoint => {
@@ -40,18 +50,23 @@ class EndPointscontroller {
 
     getEndPoint(endPoint) {
         return new Promise((resolve, reject) => {
-            $.ajax({
-                url: 'https://api.ngrok.com/tunnels',
-                beforeSend: function (request) {
-                    request.setRequestHeader("Authorization", `Bearer ${endPoint.token}`);
-                    request.setRequestHeader("Ngrok-Version", 2);
-                },
-            }).then((data) => {
-                if (data.tunnels && data.tunnels.length) {
-                    endPoint.url = data.tunnels[0].public_url;
-                }
+            if (endPoint.url) {
                 resolve(endPoint);
-            });
+            }
+            else {
+                $.ajax({
+                    url: 'https://api.ngrok.com/tunnels',
+                    beforeSend: function (request) {
+                        request.setRequestHeader("Authorization", `Bearer ${endPoint.token}`);
+                        request.setRequestHeader("Ngrok-Version", 2);
+                    },
+                }).then((data) => {
+                    if (data.tunnels && data.tunnels.length) {
+                        endPoint.url = data.tunnels[0].public_url;
+                    }
+                    resolve(endPoint);
+                });
+            }
         })
     }
 }
